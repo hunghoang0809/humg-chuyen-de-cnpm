f25e600b56fdfe6119e5a7a62cf134ca099a334e
Duong Pham <t0phtr00@students.oamk.fi>
2023-10-12 19:32:01 +0200

    Login function

--- login-app-backend/controllers/auth.controllers.js
+++ login-app-backend/controllers/auth.controllers.js
@@ -1,1 +1,34 @@
-const express = require("express")
+import connection from "../database.js"
+
+async function Login(req, res, next) {
+  const { username, password } = req.body
+
+  let sqlUserSelect =
+    "SELECT DISTINCT * FROM users WHERE user_name = ? AND password = ?"
+  connection.query(
+    sqlUserSelect,
+    [username, password],
+
+    function (err, result) {
+      if (err) {
+        console.log(err)
+        return
+      }
+
+      if (!result)
+        throw res.status(400).json("Tên đăng nhập hoặc mật khẩu không đúng")
+
+      res.cookie("id", result[0].id)
+
+      return res.status(200).json(result)
+    }
+  )
+}
+
+async function Register() {}
+
+async function Logout() {}
+
+async function GetUserById() {}
+
+export { Login, Register, Logout, GetUserById }
--- login-app-backend/routes/auth.route.js
+++ login-app-backend/routes/auth.route.js
@@ -1,4 +1,17 @@
 const express = require("express")
 const router = express.Router()
 
-router.post("/login")
+import {
+  Login,
+  Register,
+  Logout,
+  GetUserById,
+} from "../controllers/auth.controllers.js"
+import { checkLogin } from "../middlewares/checkLogin.js"
+
+router.post("/login", Login)
+router.post("/register", Register)
+router.post("/logout", Logout)
+router.get("/user/:id", checkLogin, GetUserById)
+
+module.exports = router
