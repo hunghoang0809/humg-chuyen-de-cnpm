b6924596757b0a5b76a21f71950efee73ce2f3f2
Hung Hoang <hoangmanhhung008009@gmail.com>
2023-10-13 14:44:58 +0700

    rs conflig

--- login-app-backend/app.js
+++ login-app-backend/app.js
@@ -24,16 +24,7 @@ const mysql = require("mysql2")
 })
 
 
-connection.connect(function (err) {
-  if (err) throw err
-  let sql =
-    "CREATE TABLE IF NOT EXISTS users (id int NOT NULL UNIQUE AUTO_INCREMENT, fullname VARCHAR(255), user_name VARCHAR(255), password VARCHAR(255), email VARCHAR(255))"
-  connection.query(sql, function (err, result) {
-    if (err) throw err
-    console.log("Table created")
-  })
-  console.log("Connected!")
-})
+
 
 const LoginRoute = require("./routes/auth.route")
 app.use("/auth",LoginRoute)
